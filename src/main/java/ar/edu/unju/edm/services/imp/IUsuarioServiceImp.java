48a3aeedb2c84c2d5c7b96a9d8e43886741bb1da
Leonel19Hub <leonel19coraite@gmail.com>
2022-06-07 17:14:09 -0300

    usuarios: controller, reository, service e imp

--- src/main/java/ar/edu/unju/edm/services/imp/IUsuarioServiceImp.java
+++ src/main/java/ar/edu/unju/edm/services/imp/IUsuarioServiceImp.java
@@ -3,75 +3,96 @@ package ar.edu.unju.edm.services.imp;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import ar.edu.unju.edm.Controller.UsuarioController;
 import ar.edu.unju.edm.model.Usuario;
+import ar.edu.unju.edm.repository.UsuarioRepository;
 import ar.edu.unju.edm.services.IUsuarioService;
 import ar.edu.unju.edm.util.ListadoUsuario;
 
 @Service
 public class IUsuarioServiceImp implements IUsuarioService{
 
+    private static final Log GRUPO05 = LogFactory.getLog(UsuarioController.class);
+	
+	@Autowired
+	ListadoUsuario lista;
+
     @Autowired
-    ListadoUsuario listadoUsuario;
+    UsuarioRepository usuarioRepository;
 
     @Override
-    public void guardarUsuario(Usuario userToSave) {
-        userToSave.setEstado(true);
-        listadoUsuario.getListado().add(userToSave);
+    public void guardarUsuario(Usuario usuario) {
+        
+        usuario.setEstado(true);
+		// lista.getListadoUsuario().add(usuario);
+        usuarioRepository.save(usuario);
         
     }
 
     @Override
-    public ListadoUsuario mostrarUsuarios() {
-
-        // List <Usaurio> listadoAuxiliar = new ArrayList();
-        ListadoUsuario auxiliar = new ListadoUsuario();
-
-        for(int i=0;i < listadoUsuario.getListado().size();i++){
-            if (listadoUsuario.getListado().get(i).getEstado() == true) {
-                // auxiliar.add(listadoUsuario.getListado().get(i));
-            }
-        }
-
-        return auxiliar;
-    }
-
-    @Override
-    public void eliminarUsuario(Long dni) {
+    public void eliminarUsuario(Integer id) throws Exception {
         
+        // for (int i = 0; i < lista.getListadoUsuario().size(); i++) {			
+		// 	if (lista.getListadoUsuario().get(i).getDni().equals(id)) {				
+		// 		lista.getListadoUsuario().get(i).setEstado(false);		
+		// 	}            
+        // }	
         Usuario auxiliar = new Usuario();
-
-        auxiliar =  buscarUsuario(dni);
-
+        auxiliar = buscarUsuario(id);
         auxiliar.setEstado(false);
+        usuarioRepository.save(auxiliar);
         
     }
 
     @Override
-    public void modificarrUsuario(Usuario userToEdit) {
+    public void modificarUsuario(Usuario usuario) {
         
-        for(int i=0;i<listadoUsuario.getListado().size();i++){
-            if(listadoUsuario.getListado().get(i).getDni().equals(userToEdit.getDni())){
-                listadoUsuario.getListado().set(1, userToEdit);
-            }
-        }
+        // for (int i = 0; i < lista.getListadoUsuario().size(); i++) {			
+		// 	if (lista.getListadoUsuario().get(i).getDni().equals(usuario.getDni())) {
+		// 		GRUPO05.error("encontrando usuario "+usuario.getDni());
+		// 		lista.getListadoUsuario().set(i, usuario);			
+		// 	}            
+        // }
+        usuarioRepository.save(usuario);
         
     }
 
     @Override
-    public Usuario buscarUsuario(Long dni) {
-
-        Usuario auxiliar = new Usuario();
-
-        for(int i=0;i < listadoUsuario.getListado().size();i++){
-            if (listadoUsuario.getListado().get(i).getDni().equals(dni)) {
-                auxiliar =  listadoUsuario.getListado().get(i);
-            }
-        }
+    public List<Usuario> listarUsuarios() {
         
-        return auxiliar;
+        List<Usuario> auxiliar = new ArrayList<>();
+        auxiliar = (List<Usuario>) usuarioRepository.findAll();
+        List<Usuario> auxiliar2 = new ArrayList<>();
+		// GRUPO05.info("ingresando al metodo: listarUsuarios");
+		for (int i = 0; i < auxiliar.size(); i++) {
+			// GRUPO05.error("recorriendo: listadoUsuario"+auxiliar.get(i).getDni());
+			
+			if (auxiliar.get(i).getEstado()==true) {
+				auxiliar2.add(auxiliar.get(i));		
+			}            
+        }
+        // auxiliar = (List<Usuario>) usuarioRepository.findAll();
+		return auxiliar2;
     }
-    
+
+    @Override
+    public Usuario buscarUsuario(Integer id) throws Exception{
+        
+        Usuario usuarioEncontrado = new Usuario();
+		// for (int i = 0; i < lista.getListadoUsuario().size(); i++) {
+			
+		// 	if (lista.getListadoUsuario().get(i).getDni().equals(id)) {
+
+		// 		usuarioEncontrado = lista.getListadoUsuario().get(i);		
+		// 	}            
+        // }
+
+        usuarioEncontrado = usuarioRepository.findById(id).orElseThrow(()-> new Exception("Usuario no encontrado"));
+		return usuarioEncontrado;
+    }
 }
